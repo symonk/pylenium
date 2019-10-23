f2522220afe33f6620f4dd06e0195cd5efb3323c
symonk <jackofspaces@gmail.com>
2019-10-23 18:48:10 +0100

    restructure, ready for pytest merger!

--- pylenium/utility/decorators.py
+++
@@ -1,7 +0,0 @@
-
-
-def pylenium_case_information(case: str = None, issue_id: str = None, description: str = None):
-    pass
-
-def loadable(page: str):
-    pass
--- tests/integration/base_integration_test.py
+++
@@ -1,5 +0,0 @@
-
-
-class BaseIntegrationTest:
-    pass
-
