bc6d98ef95a26ee3974d9229354871f2041beb23
symonk <jackofspaces@gmail.com>
2019-12-06 22:37:45 +0000

    added unit tests for master checking on xdist

---
+++ testing/unit/test_plugin_utilities.py
@@ -0,0 +1,11 @@
+from pylenium.globals import PYTEST_XDIST_WORKER
+from pylenium.plugin_util import is_master_process
+
+
+def test_is_master_on_xdist_worker(monkeypatch):
+    monkeypatch.setenv(PYTEST_XDIST_WORKER, 'gw100')
+    assert not is_master_process()
+
+
+def test_is_master_on_master():
+    assert is_master_process()
