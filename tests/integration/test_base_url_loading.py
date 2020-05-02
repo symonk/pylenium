5adae106355b3afa61b82ba7fe238252abe661f3
symonk <jackofspaces@gmail.com>
2020-05-02 20:31:42 +0100

    add --headless for travis

--- tests/integration/test_base_url_loading.py
+++ tests/integration/test_base_url_loading.py
@@ -9,7 +9,7 @@ def test_base_url_loading_is_correct(testdir):
         """
     )
     result = testdir.runpytest(
-        "--base-url", "http://localhost:8080", "--acquire-binary"
+        "--base-url", "http://localhost:8080", "--acquire-binary", "--headless"
     )
     assert result.ret == ExitCode.OK
 
