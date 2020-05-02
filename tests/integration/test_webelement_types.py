e90be4731c66a04459af963a4c346608fdac1e03
symonk <jackofspaces@gmail.com>
2020-05-02 20:18:24 +0100

    fixed imports

--- tests/integration/test_webelement_types.py
+++ tests/integration/test_webelement_types.py
@@ -1,11 +1,9 @@
 import pytest
 from pytest import ExitCode
-from tests.integration.testdir_utilities import HTTP_SERVER_CONFTEST
 
 
 @pytest.mark.skip
 def test_web_element_types(testdir):
-    testdir.makeconftest(HTTP_SERVER_CONFTEST)
     testdir.makepyfile(
         """
         def test_element_type(pydriver):
