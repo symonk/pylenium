fbd03c57d3114c02ceb3bad3a65b2b2e58d260dc
symonk <jackofspaces@gmail.com>
2020-05-02 16:49:04 +0100

    fixing up drivers, refactoring and cleaning up chromedriverfactory

--- src/pylenium/webdriver/driver_factory.py
+++ src/pylenium/webdriver/driver_factory.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from abc import ABC
 from abc import abstractmethod
+from typing import Any
+
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
@@ -8,6 +10,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class AbstractDriverFactory(ABC):
+    def __init__(self, config):
+        self.config = config
+        self.base_url = self._config_pluck("base_url")
+        self.headless = self._config_pluck("headless")
+
     @abstractmethod
     def create_driver(self) -> WebDriver:
         """
@@ -16,20 +23,58 @@ class AbstractDriverFactory(ABC):
         :return: an instance of remote web driver
         """
 
+    def _config_pluck(self, option) -> Any:
+        """
+        Quick way to retrieve a value from the pytest config specifying a default if it does not exist
+        :param option: the option (str) to go hunting for
+        :return: the value stored in the config options for the given option (key)
+        """
+        return self.config.getoption(option)
+
 
 class ChromeDriverFactory(AbstractDriverFactory):
     def __init__(self, config):
-        self.config = config
-        super().__init__()
+        super().__init__(config)
+        self.chrome_options = ChromeOptions()
 
     def create_driver(self) -> WebDriver:
-        chrome_options = ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        driver = ChromeDriver(
-            executable_path=ChromeDriverManager().install(), options=chrome_options
-        )
-        base_url = self.config.getoption("base_url")
-        if base_url:
-            driver.get(base_url)
+        """
+        Chrome driver creation
+        :return: the instantiated instance of the chrome driver
+        """
+        self._resolve_options()
+        binary_path = self._resolve_driver_path()
+        driver = ChromeDriver(executable_path=binary_path, options=self.chrome_options)
+        return self._load_base_url_if_necessary(driver)
+
+    def _load_base_url_if_necessary(self, driver) -> WebDriver:
+        """
+        Checks --base-url for a loadable url and loads it prior to returning the driver
+        :param driver: the driver instances; recently instantiated
+        :return: the driver for fluency
+        """
+        if self.base_url is not None:
+            driver.get(self.base_url)
         return driver
+
+    def _resolve_driver_path(self) -> str:
+        """
+        Decide if we need to:
+        A) Acquire the driver binary if --acquire binary was provided
+        B) Use a local path passed by the user through --driver-binary-path
+        note: Precedence here is that --acquire-binary is king, followed by path
+        note: Driver-binary-path is validated at plugin load time, if we cannot find it we won't get this far
+        :return: the path to the chrome-driver binary - downloaded or installed by the user
+        """
+        if self._config_pluck("acquire_binary"):
+            return ChromeDriverManager().install()
+        return self._config_pluck("driver_binary_path")
+
+    def _resolve_options(self) -> None:
+        """
+        Build up chrome options through what is passed to the CLI as well as what is provided on the command line args
+        :return: None; this is done in-place on self.chrome_options
+        """
+        if self.headless:
+            self.chrome_options.add_argument("--headless")
+            self.chrome_options.add_argument("--disable-gpu")
