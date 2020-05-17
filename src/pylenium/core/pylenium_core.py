08346d4acdf68a1368d9f6a45add6a55706405d9
symonk <jackofspaces@gmail.com>
2020-05-17 13:26:30 +0100

    adding our own locator encapulsations

--- src/pylenium/core/pylenium_core.py
+++ src/pylenium/core/pylenium_core.py
@@ -1,28 +1,83 @@
-from typing import Callable
+from dataclasses import dataclass
+from selenium.webdriver.common.by import By
 
 
-def _locatable(selector: str, locator_type: id) -> Callable:
+@dataclass(frozen=True)
+class Locatable:
+    by: By
+    selector: str
+
+    def __call__(self):
+        pass
+
+
+def id(selector: str) -> Locatable:
     """
-    Lazily used in page objects to store a locator, this returns a callable used by the driver when the time comes to
-    actually interact with the element
-    :return:
+    Pylenium ID locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
     """
-    return lambda: 0
+    return Locatable(By.ID, selector)
 
 
-def xpath(selector: str) -> Callable:
+def xpath(selector: str) -> Locatable:
     """
     Pylenium xpath locator
     :param selector: the string of the selector for us to do lookup of
-    :return: an instance of callable that can be used to find the element
+    :return: an instance of Locator (By and selector encapsulated together)
     """
-    return _locatable(selector, "xpath")
+    return Locatable(By.XPATH, selector)
+
+
+def link_text(selector: str) -> Locatable:
+    """
+    Pylenium link text locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
+    """
+    return Locatable(By.LINK_TEXT, selector)
+
+
+def partial_link_text(selector: str) -> Locatable:
+    """
+    Pylenium partial link text locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
+    """
+    return Locatable(By.PARTIAL_LINK_TEXT, selector)
+
+
+def name(selector: str) -> Locatable:
+    """
+    Pylenium name locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
+    """
+    return Locatable(By.NAME, selector)
+
+
+def tag_name(selector: str) -> Locatable:
+    """
+    Pylenium tag name locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
+    """
+    return Locatable(By.TAG_NAME, selector)
+
+
+def class_name(selector: str) -> Locatable:
+    """
+    Pylenium class name locator
+    :param selector: the string of the selector for us to do lookup of
+    :return: an instance of Locator (By and selector encapsulated together)
+    """
+    return Locatable(By.CLASS_NAME, selector)
 
 
 def css(selector: str):
     """
     Pylenium css locator
     :param selector: the string of the selector for us to do lookup of
-    :return: an instance of callable that can be used to find the element
+    :return: an instance of Locator (By and selector encapsulated together)
     """
-    return _locatable(selector, "css")
+    return Locatable(By.CSS_SELECTOR, selector)
